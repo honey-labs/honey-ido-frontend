50353d0fd4ab4a6390c2378bc18baa544b7775f4
Tracy <tracy@tracycodes.com>
2022-01-03 11:38:42 -0700

    Update FAQs

--- src/components/ido/Faqs.tsx
+++ src/components/ido/Faqs.tsx
@@ -78,27 +78,29 @@ const faqs = [
           This can be caused by numerous different factors. The following steps
           are a good starting point to resolve the issue:
         </p>
-        <ul>
-          <li>
-            Check that you have SOL in the wallet you&apos;re using. If you do
-            not, try sending around 0.1 SOL to your wallet.
-          </li>
-          <li>
-            Check that you&apos;re using the latest version of
-            Chrome/Brave/Firefox.
-          </li>
-          <li>
-            Try a different network (VPN, connect to a mobile hotspot, etc)
-          </li>
-          <li>
-            Try a different web browser. Example: if using Chrome, try Firefox.
-          </li>
-          <li>Try small test transactions to see if that goes through.</li>
-          <li>
-            Shift your balance to a different wallet and try from that different
-            wallet.
-          </li>
-        </ul>
+        <br />
+        <p>
+          Check that you have SOL in the wallet you&apos;re using. If you do
+          not, try sending around 0.1 SOL to your wallet.
+        </p>
+        <br />
+        <p>
+          Check that you&apos;re using the latest version of
+          Chrome/Brave/Firefox.
+        </p>
+        <br />
+        <p>Try a different network (VPN, connect to a mobile hotspot, etc)</p>
+        <br />
+        <p>
+          Try a different web browser. Example: if using Chrome, try Firefox.
+        </p>
+        <br />
+        <p>Try small test transactions to see if that goes through.</p>
+        <p>
+          Shift your balance to a different wallet and try from that different
+          wallet.
+        </p>
+        <br />
       </>
     ),
   },
