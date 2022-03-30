e41ec56834182df768be5c20456e0245acbba1f2
Oshioke Iyogwoya <danieloshos3@gmail.com>
2022-03-30 13:53:03 +0100

    pool countdown ui fixes

--- src/components/ido/PoolCountdown.tsx
+++ src/components/ido/PoolCountdown.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import { Box, Text } from 'degen'
 import moment from 'moment'
 import Countdown from 'react-countdown'
 
@@ -20,29 +21,76 @@ const PoolCountdown: React.FC<PoolCountdownProps> = ({
   const renderCountdown = ({ days, hours, minutes, seconds, completed }) => {
     hours += days * 24
     if (completed) {
-      return <p className="text-sm mt-2 py-2 text-center">{poolStatus}</p>
+      return (
+        <Box marginTop="2" paddingY="2">
+          <Text size="small">{poolStatus}</Text>
+        </Box>
+      )
     } else {
       return (
-        <div className={classNames(className, 'flex items-center')}>
-          <div className="flex flex-col items-center">
-            <span className="bg-black text-white text-center font-bold mx-1 w-8 inline-block py-2 rounded">
-              {hours < 10 ? `0${hours}` : hours}
-            </span>
-            <span className="text-xs mt-1 text-secondary">hrs</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="bg-black text-white text-center font-bold mx-1 w-8 inline-block py-2 rounded">
-              {minutes < 10 ? `0${minutes}` : minutes}
-            </span>
-            <span className="text-xs mt-1 text-secondary">mins</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="bg-black text-white text-center font-bold mx-1 w-8 inline-block py-2 rounded">
-              {seconds < 10 ? `0${seconds}` : seconds}
-            </span>
-            <span className="text-xs mt-1 text-secondary">secs</span>
-          </div>
-        </div>
+        <Box display="flex" alignItems="center">
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Box
+              as="span"
+              backgroundColor="black"
+              textAlign="center"
+              marginX="1"
+              marginBottom="1"
+              width="8"
+              display="inline-block"
+              paddingY="2"
+              borderRadius="medium"
+            >
+              <Text as="span" weight="bold" color="text">
+                {hours < 10 ? `0${hours}` : hours}
+              </Text>
+            </Box>
+            <Text as="span" size="small" color="textSecondary">
+              hrs
+            </Text>
+          </Box>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Box
+              as="span"
+              backgroundColor="black"
+              textAlign="center"
+              marginX="1"
+              marginBottom="1"
+              width="8"
+              display="inline-block"
+              paddingY="2"
+              borderRadius="medium"
+            >
+              <Text as="span" weight="bold" color="text">
+                {minutes < 10 ? `0${minutes}` : minutes}
+              </Text>
+            </Box>
+            <Text as="span" size="small" color="textSecondary">
+              mins
+            </Text>
+          </Box>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Box
+              as="span"
+              backgroundColor="black"
+              textAlign="center"
+              marginX="1"
+              marginBottom="1"
+              width="8"
+              display="inline-block"
+              paddingY="2"
+              borderRadius="medium"
+            >
+              <Text as="span" weight="bold" color="text">
+                {seconds < 10 ? `0${seconds}` : seconds}
+              </Text>
+            </Box>
+
+            <Text as="span" size="small" color="textSecondary">
+              secs
+            </Text>
+          </Box>
+        </Box>
       )
     }
   }
